394f225181abc130bd788180bf757a495402ec75
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-01-26 18:18:20 +0100

    updated masterchef interface

--- contracts/interfaces/IMasterChefv2.sol
+++ contracts/interfaces/IMasterChefv2.sol
@@ -2,22 +2,34 @@
 
 pragma solidity 0.8.9;
 
-import './IMasterChef.sol';
+interface IMasterChefv2 {
 
-interface IMasterChefv2 is IMasterChef {
-    function harvest(uint256 pid, address to) external;
+    function userInfo(uint256 _pid, address _user) external view returns (uint256, uint256);
 
-    function withdraw(
-        uint256 pid,
-        uint256 amount,
-        address to
-    ) external;
-
-    function deposit(
-        uint256 pid,
-        uint256 amount,
-        address to
-    ) external;
+    function poolLength() external view returns (uint256);
 
     function lqdrPerBlock() external view returns (uint256);
+
+    function getMultiplier(uint256 _from, uint256 _to) external view returns (uint256);
+
+    function pendingLqdr(uint256 _pid, address _user) external view returns (uint256);
+
+    function massUpdatePools() external;
+
+    function updatePool(uint256 _pid) external;
+
+    function massHarvestFromStrategies() external;
+
+    function emergencyWithdraw(uint256 _pid, address _to) external;
+
+    function withdrawAndHarvest(uint256 _pid, uint256 _amount, address _to) external;
+
+    function harvest(uint256 pid, address to) external;
+
+    function harvestFromMasterChef() external;
+
+    function withdraw(uint256 pid, uint256 amount, address to) external;
+
+    function deposit(uint256 pid, uint256 amount, address to) external;
+
 }
