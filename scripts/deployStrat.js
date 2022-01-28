8615369a6441c650165fcc50ca59be3b76eb31e4
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-01-28 14:45:11 +0100

    fine tuning deployment

---
+++ scripts/deployStrat.js
@@ -0,0 +1,33 @@
+const deployStrategy = async (vaultAddress) => {
+  const Strategy = await ethers.getContractFactory("ReaperAutoCompound_LiquidV2_Beethoven");
+  const treasuryAddress = "0x0e7c5313E9BB80b654734d9b7aB1FB01468deE3b";
+  const paymentSplitterAddress = "0x63cbd4134c2253041F370472c130e92daE4Ff174";
+  const strategists = ["0x1E71AEE6081f62053123140aacC7a06021D77348","0x81876677843D00a7D792E1617459aC2E93202576","0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4"];
+  const wantAddress = "0xf3A602d30dcB723A74a0198313a7551FEacA7DAc";
+  const poolId = 31;
+  const underlyingWeights = [2500,2500,2500,2500];
+
+  const strategy = await Strategy.deploy(
+    vaultAddress,
+    [treasuryAddress,paymentSplitterAddress],
+    strategists,
+    wantAddress,
+    poolId,
+    underlyingWeights
+  );
+  await strategy.deployed();
+  console.log("Strategy deployed to:", strategy.address);
+  return strategy.address;
+}
+
+async function main() {
+  const vaultAddress = "0x25af6Bb8b9308b3B187F4c67344031b4bBdf8d44";
+  const strategyAddress = await deployStrategy(vaultAddress);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- scripts/initialize.js
+++ scripts/initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = "0x25af6Bb8b9308b3B187F4c67344031b4bBdf8d44";
+  const strategyAddress = '0xe548E2428A32c317DbbFaB0716C9329E1F435034';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
   const vault = Vault.attach(vaultAddress);
